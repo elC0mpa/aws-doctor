463e797fc5d8712ba1ac250dbc06623dc96b429d
danyer94 <danyerag@gmail.com>
2026-02-03 13:28:51 -0500

    refactor: enhance banner color management
    - Introduced a new `BannerColor` type and a set of constants for various banner colors, improving the flexibility and readability of color management.
    - Updated the `DrawBannerTitle` function to utilize an array of ANSI color codes instead of a single constant, allowing for easier customization of the banner title color.

--- utils/banner.go
+++ utils/banner.go
@@ -8,7 +8,55 @@ import (
 	"golang.org/x/term"
 )
 
-const bannerTitleColor = "\x1b[38;2;80;160;255m"
+type BannerColor int
+
+const (
+	BannerCocaColaRed BannerColor = iota
+	BannerFacebookBlue
+	BannerTwitterBlue
+	BannerLinkedInBlue
+	BannerIBMBlue
+	BannerYouTubeRed
+	BannerSpotifyGreen
+	BannerNetflixRed
+	BannerTwitchPurple
+	BannerYahooPurple
+	BannerAmazonOrange
+	BannerIntelBlue
+	BannerWhatsAppGreen
+	BannerAndroidGreen
+	BannerSkypeBlue
+	BannerStarbucksGreen
+	BannerPinterestRed
+	BannerAirbnbPink
+	BannerFantaOrange
+	BannerBMWBlue
+)
+
+var bannerTitleColors = []string{
+	"\x1b[38;2;228;0;43m",   // Coca-Cola Red
+	"\x1b[38;2;24;119;242m", // Facebook Blue
+	"\x1b[38;2;29;161;242m", // Twitter/X Blue
+	"\x1b[38;2;10;102;194m", // LinkedIn Blue
+	"\x1b[38;2;15;98;254m",  // IBM Blue
+	"\x1b[38;2;255;0;0m",    // YouTube Red
+	"\x1b[38;2;30;215;96m",  // Spotify Green
+	"\x1b[38;2;229;9;20m",   // Netflix Red
+	"\x1b[38;2;145;70;255m", // Twitch Purple
+	"\x1b[38;2;95;39;205m",  // Yahoo Purple
+	"\x1b[38;2;255;153;0m",  // Amazon Orange
+	"\x1b[38;2;0;113;197m",  // Intel Blue
+	"\x1b[38;2;37;211;102m", // WhatsApp Green
+	"\x1b[38;2;61;220;132m", // Android Green
+	"\x1b[38;2;0;175;240m",  // Skype Blue
+	"\x1b[38;2;0;112;74m",   // Starbucks Green
+	"\x1b[38;2;189;8;28m",   // Pinterest Red
+	"\x1b[38;2;255;90;95m",  // Airbnb Pink
+	"\x1b[38;2;255;114;0m",  // Fanta Orange
+	"\x1b[38;2;0;152;218m",  // BMW Blue
+}
+
+const bannerTitleColor = BannerSkypeBlue
 
 var titleLines = []string{
 "  █████╗  ██╗    ██╗ ███████╗        ██████╗   ██████╗   ██████╗ ████████╗  ██████╗  ██████╗ ",
@@ -40,7 +88,7 @@ func DrawBannerTitle() {
 		width = w
 	}
 
-	fmt.Print(bannerTitleColor)
+	fmt.Print(bannerTitleColors[bannerTitleColor])
 	printCenteredLines(titleLines, width)
 	fmt.Print("\x1b[0m")
 }
