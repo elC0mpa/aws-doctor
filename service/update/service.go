3770f57a2713b674aadd3431e3f6d6b876b20708
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2026-02-04 12:18:05 +0400

    test(service/update): increase coverage for update service to 100%
    Refactored update service to use an internal commandRunner interface
    to allow mocking of shell commands. Added unit tests for successful
    and failed update scenarios.

--- service/update/service.go
+++ service/update/service.go
@@ -6,19 +6,26 @@ import (
 	"os/exec"
 )
 
+type realRunner struct{}
+
+func (r *realRunner) Run(name string, arg ...string) error {
+	cmd := exec.Command(name, arg...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	return cmd.Run()
+}
+
 // NewService creates a new update service.
 func NewService() Service {
-	return &service{}
+	return &service{
+		runner: &realRunner{},
+	}
 }
 
 func (s *service) Update() error {
 	// Reutilize the install.sh script from the repository
-	cmd := exec.Command("sh", "-c", "curl -sSL https://raw.githubusercontent.com/elC0mpa/aws-doctor/main/install.sh | sh")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
-
-	if err := cmd.Run(); err != nil {
+	if err := s.runner.Run("sh", "-c", "curl -sSL https://raw.githubusercontent.com/elC0mpa/aws-doctor/main/install.sh | sh"); err != nil {
 		return fmt.Errorf("failed to run update script: %w", err)
 	}
 
