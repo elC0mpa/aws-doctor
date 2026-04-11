e5cb0a0b1f3244f328f7db00d27dadcd5ec84e03
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2026-04-11 18:09:46 +0400

    fix(version): improved implementation for isEqual in version

--- utils/version/version.go
+++ utils/version/version.go
@@ -3,22 +3,9 @@ package version
 import "strings"
 
 // IsEqual compares the latest tag from GitHub with the local version.
-// It returns true if:
-// 1. Both strings are exactly equal.
-// 2. The tag equals the local version after removing its first character (e.g., 'v' prefix).
 func IsEqual(latestTag, localVersion string) bool {
-	if latestTag == localVersion {
-		return true
-	}
+	l := strings.TrimPrefix(strings.ToLower(latestTag), "v")
+	v := strings.TrimPrefix(strings.ToLower(localVersion), "v")
 
-	if len(latestTag) > 0 && latestTag[1:] == localVersion {
-		return true
-	}
-
-	return false
-}
-
-// HasVPrefix checks if the string starts with 'v'.
-func HasVPrefix(s string) bool {
-	return strings.HasPrefix(strings.ToLower(s), "v")
+	return l == v
 }
